e60de599abc81d556e554599aae969ed491e3232
Sandeep Reddy <mandasandeepreddy18@gmail.com>
2025-10-24 23:35:19 +0530

    Create 2048_Next-Greater-Numerically-Balanced-Number.py

---
+++ LeetCode/2048_Next-Greater-Numerically-Balanced-Number.py
@@ -0,0 +1,23 @@
+class Solution:
+    def nextBeautifulNumber(self, n: int) -> int:
+        def isBalance(number):
+            number_str = str(number)
+            counts = {}
+            for i in number_str:
+                if i in counts:
+                    counts[i] += 1 
+                else:
+                    counts[i] = 1
+
+            
+            for i in counts:
+                if int(i) != counts[i]:
+                    return False
+
+            return True
+
+        n+=1
+        while not isBalance(n):
+            n+=1
+        
+        return n
